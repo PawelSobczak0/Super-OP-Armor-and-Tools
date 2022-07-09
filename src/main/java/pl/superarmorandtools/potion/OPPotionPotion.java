4da0312465a3505ecbdb207aab7bf04b7a55600f
PawelSobczak0 <Tomek@DESKTOP-HF7KGO6.home>
2022-07-09 12:51:42 +0200

    Added Potion and command /god

---
+++ src/main/java/pl/superarmorandtools/potion/OPPotionPotion.java
@@ -0,0 +1,28 @@
+
+package pl.superarmorandtools.potion;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.RegistryEvent;
+
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.EffectInstance;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class OPPotionPotion {
+	@ObjectHolder("super_armor_and_tools:op_potion")
+	public static final Potion potionType = null;
+
+	@SubscribeEvent
+	public static void registerPotion(RegistryEvent.Register<Potion> event) {
+		event.getRegistry().register(new PotionCustom());
+	}
+
+	public static class PotionCustom extends Potion {
+		public PotionCustom() {
+			super(new EffectInstance(OPPotionEffect.potion, 3600, 0, false, false));
+			setRegistryName("op_potion");
+		}
+	}
+}
---
+++ src/main/java/pl/superarmorandtools/procedures/OPEffectStartedappliedProcedure.java
@@ -0,0 +1,24 @@
+package pl.superarmorandtools.procedures;
+
+import pl.superarmorandtools.SuperArmorAndToolsMod;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.Entity;
+
+import java.util.Map;
+
+public class OPEffectStartedappliedProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				SuperArmorAndToolsMod.LOGGER.warn("Failed to load dependency entity for procedure OPEffectStartedapplied!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		if (entity instanceof PlayerEntity) {
+			((PlayerEntity) entity).abilities.disableDamage = (true);
+			((PlayerEntity) entity).sendPlayerAbilities();
+		}
+	}
+}
